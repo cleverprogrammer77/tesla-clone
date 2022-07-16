5ad6ac309c0d88529c8225838710eb908b0a0461
priyanshu Jha <priyanshujha14@gmail.com>
2022-07-16 19:41:28 +0530

    tesla website with react js

---
+++ src/components/Home.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order online for touchless delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order online for touchless delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order online for touchless delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order online for touchless delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Lowest cost solar panels in India"
+        description="Money back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop Now"
+      />
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
---
+++ src/components/Section.js
@@ -0,0 +1,104 @@
+import React from "react";
+import styled from "styled-components";
+import Fade from "react-reveal/Fade";
+
+function Section({
+  title,
+  description,
+  backgroundImg,
+  leftBtnText,
+  rightBtnText,
+}) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <Fade top>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description} </p>
+        </ItemText>
+      </Fade>
+      <Button>
+        <Fade top>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+        </Fade>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Button>
+    </Wrap>
+  );
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/model-s.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; //vertical
+  align-items: center; // horizontal
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-tranform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const RightButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  // color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-tranform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+  background: white;
+  color: black;
+  opacity: 0.65;
+`;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
+
+const Button = styled.div``;
